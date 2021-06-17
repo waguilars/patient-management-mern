75902fe2ea5a066cee625e4da3d75665bc44a0ef
Wilson Aguilar <waguilars@est.ups.edu.ec>
2021-06-17 13:58:02 -0500

    electron app is ready

--- desktop/index.js
+++ desktop/index.js
@@ -1,8 +1,28 @@
 const { app, BrowserWindow } = require('electron');
 
+let appWindow;
 
 function crearVentana() {
+  appWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    minWidth: 800,
+    minHeight: 600,
+    center: true,
+    show: false,
+    icon: 'icon.png'
+  });
 
+  // Cuando la app se cierra
+  appWindow.on('closed', () => {
+    appWindow = null;
+  });
+
+  appWindow.loadFile('./index.html');
+
+  appWindow.once('ready-to-show', () => {
+    appWindow.show();
+  });
 }
 
-app.on('ready', crearVentana)
+app.on('ready', crearVentana);
---
+++ desktop/js/app.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('http://localhost:4000/pacientes')
+    .then((resp) => resp.json())
+    .then((datos) => mostrarCitas(datos.pacientes));
+});
+
+function mostrarCitas(citas) {
+  const citasContainer = document.querySelector('#citas');
+
+  let citasHTML = '';
+
+  citas.forEach((cita) => {
+    citasHTML += `
+      <div class="p-5 list-group-item list-group-item-action flex-column align-items-start">
+                  <div class="d-flex w-100 justify-content-between mb-4">
+                    <h3 class="mb-3">${cita.nombre}</h3>
+                    <small class="fecha-alta">${cita.fecha} - ${cita.hora}</small>
+                  </div>
+
+                  <p class="mb-0">
+                    ${cita.sintomas}
+                  </p>
+                  <div class="contacto py-3">
+                    <p> Dueño: ${cita.propietario}</p>
+                    <p> Telefono: ${cita.telefono}</p>
+                  </div>
+      </div>
+    `;
+  });
+
+  citasContainer.innerHTML = citasHTML;
+}
